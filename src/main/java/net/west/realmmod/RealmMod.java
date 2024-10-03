9d9460f945b13613b1130b7f35ebe770403fea74
multidatapack@hotmail.com <multidatapack@hotmail.com>
2024-10-02 19:29:46 -0700

    TEST

---
+++ src/main/java/net/west/realmmod/RealmMod.java
@@ -0,0 +1,18 @@
+package net.west.realmmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RealmMod implements ModInitializer {
+	public static final String MOD_ID = "realmmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
